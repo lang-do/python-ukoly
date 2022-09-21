ed4be4c7f8a37e62807f4b1b409cad7329acab75
lang-do <do.langova@gmail.com>
2022-09-21 20:37:32 +0200

    ukol01_DominikaLangova

--- ukol-1.py
+++ ukol-1.py
@@ -1,1 +1,13 @@
-#TODO
+baliky = {
+    "B541X": True,
+    "B547X": False,
+    "B251X": False,
+    "B501X": True,
+    "B947X": False,
+}
+
+kodBaliku = input("Zadejte kód balíku: ")
+if kodBaliku in baliky:
+    print("Balík byl předán kurýrovi.")
+else:
+    print("Balík zatím nebyl předán kurýrovi.")
