5203e76d7b9ea2ca57d3548027a86a8abc399b92
lang-do <do.langova@gmail.com>
2022-10-18 15:46:59 +0200

    ukol03_DominikaLangova

--- ukol-3.py
+++ ukol-3.py
@@ -1,4 +1,5 @@
 import math
+
 def validate_number(tel_num=False):
 	tel_num = input('Zadejte telefonni cislo adresata: ')
 	tel_num = tel_num.replace(' ', '')
@@ -7,11 +8,14 @@ def validate_number(tel_num=False):
 	elif len(tel_num) == 13 and tel_num[0:4] == '+420':
 		return True
 	else:
-		print('Chybny format telefonniho cisla.')
-def price(message, tel_num):
-    message = input('Zadejte text zpravy: ')
-    num_char = len(message)
-    num_messages = math.ceil(num_char / 180)
-    if tel_num:
-        return num_messages * 3
-print(f'Cena zpravy je {price(input, True)} Kč.')
+		return False
+		
+if validate_number(True):
+	def price(message):
+		message = input('Zadejte text zpravy: ')
+		num_char = len(message)
+		num_messages = math.ceil(num_char / 180)
+		return num_messages * 3
+	print(f'Cena zpravy je {price(input)} Kč.')
+else:
+	print('Chybny format telefonniho cisla.')
