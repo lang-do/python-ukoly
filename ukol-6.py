fb96872190db38af6b04483c08367ba27c1fa971
lang-do <do.langova@gmail.com>
2022-11-07 17:09:37 +0100

    ukol06_DominikaLangova

--- ukol-6.py
+++ ukol-6.py
@@ -1,10 +1,11 @@
-with open('znamky.txt', 'r', encoding='utf-8') as file:
-    znamky = file.readlines()
+r = {'1':'A', '2':'B', '3':'C', '4':'D', '5':'F'}
 
-with open('file.txt', 'r') as file :
-  filedata = file.read()
-
-znamky = znamky.replace('1', 'A')
-
-with open('file.txt', 'w') as file:
-  file.write(znamky)
+lines = []
+with open('znamky.txt') as infile:
+    for line in infile:
+        for src, target in r.items():
+            line = line.replace(src, target)
+        lines.append(line)
+with open('znamky.txt', 'w') as outfile:
+    for line in lines:
+        outfile.write(line)
